a5770cbc43ae211d319d918b4eb133482baac26c
Mbicha <mbithicharlse@gmail.com>
2023-01-12 20:48:16 +0300

    Created a new script 1-country_users.sql

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- Create table users
+
+
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
